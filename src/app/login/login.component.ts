6444232bae718be30c8d41d0cf78e1ef94dd5699
BafS <fabacrans@gmail.com>
2016-10-13 22:45:07 +0200

    Send authentication [wip]

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { MessageService } from './../shared/services/api/message.service'
+import { AuthenticationService } from './../shared/services/api/authentication.service'
 
 class User {
   constructor(
@@ -13,11 +15,22 @@ class User {
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [ AuthenticationService, MessageService ]
 })
 export class LoginComponent implements OnInit {
+  private _messages: any[] = [];
+  // private _messageService: MessageService;
 
-  model = new User('', '')
+  constructor (
+    private _messageService: MessageService,
+    private _authService: AuthenticationService
+    ) {
+    this._messageService = _messageService;
+    this._authService = _authService;
+  }
+
+  user = new User('test@test.com', 'test')
 
   // constructor(fb: FormBuilder) {
     // this.loginForm = fb.group({
@@ -25,12 +38,32 @@ export class LoginComponent implements OnInit {
     //   password: ["", Validators.required]
     // });
   // }
-  doLogin(event) {
-    // console.log(this.loginForm.value);
-    // event.preventDefault();
+
+  onSubmit(event) {
+    console.log(`do login (${this.user.email})`);
+    this._authService.auth(this.user.email, this.user.password);
+    this._messageService.create({
+      message: `User ${this.user.email} is logged (or try to...)`
+    });
+
+    // TODO
+    // If logged, redirection
+    // if not, alert message
+
+    return false;
+  }
+
+  // @DEV
+  sendMessage() {
+    console.log('send a message....')
+    this._messageService.create({
+      message: `User ${this.user.email} send a message`
+    });
   }
 
   ngOnInit() {
+    // this._messageService.find().then(messages => {
+      //  this._messages = messages;
+    // });
   }
-
 }
