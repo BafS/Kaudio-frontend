18cb04aae253edcc82f464100ed59a21cf7bd672
Berich <sarra.berrich@gmail.com>
2016-10-31 23:33:16 +0100

    Registration: add html page (binding th form data to the user model) and use the UserService from the register.component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../shared/services/api/user.service'
 
+import {MessageService} from '../shared/services/api/message.service'
+
+// TODO factorisee 'User' (ds login et ds register)
+class User {
+  constructor(
+    // public id: number,
+    public email: string,
+    public password: string,
+    // public username?: string
+  ) {  }
+}
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
-})
+  styleUrls: ['./register.component.scss'],
+  providers: [ UserService, MessageService ]})
 export class RegisterComponent implements OnInit {
+    private _messages: any[] = [];
+  // private _messageService: MessageService;
+    user = new User('', '')
 
-  constructor() { }
+  constructor(private _messageService: MessageService,
+    private _userService: UserService
+    ) {
+    this._messageService = _messageService;
+    this._userService = _userService;
+  }
+  onSubmit(event) {
+    console.log(`do register (${this.user.email})`);
+
+    this._userService.create(this.user.email, this.user.password).then((result) => {
+      console.log('Registered!', result);
+
+
+
+      this._messageService.create({
+        message: `User ${this.user.email} is registered (or try to...)`
+      });
+    }).catch((error) => {
+      console.error('Error registration!', error);
+    })
+
+    // TODO
+    // If logged, redirection
+    // if not, alert message
+
+    return false;
+  }
+
 
   ngOnInit() {
   }
