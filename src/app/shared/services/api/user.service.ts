d40277566ab2b177e75341d80cccea8b63e0c677
Berich <sarra.berrich@gmail.com>
2016-10-31 23:26:24 +0100

    Add a user service to do the post (register a user)

---
+++ src/app/shared/services/api/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { RestService } from './../rest.service';
+import { SocketService } from './../socket.service';
+
+@Injectable()
+export class UserService {
+  private _socket;
+  private _rest;
+
+  constructor(
+    private _socketService: SocketService,
+    private _restService: RestService
+  ) {
+
+    this._rest = _restService.getService('user');
+    this._socket = _socketService.getService('user');
+  }
+  find(query?: any) {
+    return this._rest.find(query);
+  }
+
+  get(id: string, query: any) {
+    return this._rest.get(id, query);
+  }
+
+  create(email:string, password:string) {
+    // app.get('token')
+    return this._socket.create(email,password);
+  }
+
+  remove(id: string, query: any) {
+    return this._socket.remove(id, query);
+  }
+}
