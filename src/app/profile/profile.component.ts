cfaa3e8e3d1936e45b670af93b9a9f9b9c4442cb
Fred <frederic.fyfer@gmail.com>
2016-11-08 20:53:46 +0100

    Add profile component

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from '../shared/services/api/user.service'
+
+import {MessageService} from '../shared/services/api/message.service'
+
+// TODO factorisee 'User' (ds login et ds profile)
+class User {
+  constructor(
+    // public id: number,
+    public email: string,
+    public password: string,
+    // public username?: string
+  ) {  }
+}
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss'],
+  providers: [ UserService, MessageService ]})
+export class ProfileComponent implements OnInit {
+    private _messages: any[] = [];
+    user = new User('', '')
+
+  constructor(
+    ) {
+  }
+
+
+  ngOnInit() {
+  }
+
+}
