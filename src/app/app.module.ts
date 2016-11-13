4641354060055e2c3061a09d7d8eed9707058068
BafS <fabacrans@gmail.com>
2016-11-13 03:20:54 +0100

    Use angular2-data-table (instead of the homemade version)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { Angular2DataTableModule } from 'angular2-data-table';
 
 import { ROUTES } from './app.routes';
+
+import { RestService } from './shared/services/rest.service';
+import { SocketService } from './shared/services/socket.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './+home/home.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
-
-import { RestService } from './shared/services/rest.service';
-import { SocketService } from './shared/services/socket.service';
 import { NavigationComponent } from './shared/components/navigation/navigation.component';
 import { RegisterComponent } from './register/register.component';
 import { PlaylistComponent } from './shared/components/playlist/playlist.component';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-import { PlaylistsComponent } from './playlists/playlists.component'
+import { PlaylistsComponent } from './playlists/playlists.component';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import { PlaylistsComponent } from './playlists/playlists.component'
     FormsModule,
     HttpModule,
     RouterModule.forRoot(ROUTES, { useHash: true }),
-    AlertModule
+    AlertModule,
+    Angular2DataTableModule
   ],
   // providers: [],
   providers: [ SocketService, RestService ],
--- src/app/shared/components/playlist/playlist.component.ts
+++ src/app/shared/components/playlist/playlist.component.ts
@@ -1,14 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Piece } from '../../models'
+import { Piece } from '../../models';
 
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
-  styleUrls: ['./playlist.component.scss']
+  styleUrls: [
+    //'./../../../../../node_modules/angular2-data-table/release/datatable.css',
+    //'./../../../../../node_modules/angular2-data-table/release/material.css',
+    './playlist.component.scss'
+  ]
 })
 export class PlaylistComponent implements OnInit {
   @Input() title: string;
   @Input() pieces?: Piece[]; // songs
+
+  rows = [];
+
+  columns = [
+    { prop: 'title' },
+    { prop: 'album' },
+    { prop: 'artist' }
+  ];
 
   constructor() { }
 
