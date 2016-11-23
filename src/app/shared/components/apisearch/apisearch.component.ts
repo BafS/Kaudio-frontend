16dec53bd4b974fe80a283725ecde724faa233b3
Berich <sarra.berrich@gmail.com>
2016-11-23 01:58:54 +0100

    Search tracks in a simple way

--- src/app/shared/components/apisearch/apisearch.component.ts
+++ src/app/shared/components/apisearch/apisearch.component.ts
@@ -1,32 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import { Track } from '../../models/track';
-import { ApiSearchService } from '../../services/api/api-search.service';
-import { Subject } from 'rxjs/Subject';
+import { ApiService } from './../../services/api.service';
+import { MessageService } from './../../services/api/message.service';
+import { ApiSearchService } from './../../services/api/api-search.service';
+import { Track } from './../../models/track';
+import { Component, OnInit} from '@angular/core';
+import { TypeaheadMatch } from 'ng2-bootstrap/components/typeahead';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-search',
   templateUrl: './apisearch.component.html',
   styleUrls: ['./apisearch.component.scss'],
-  providers: [ ApiSearchService ]
+  providers: [ ApiSearchService, MessageService ]
 })
 export class ApiSearchComponent implements OnInit {
-  tracks: Track[];
-  term$ = new Subject<string>();
+  public stateCtrl: FormControl = new FormControl();
+
+  public track: Track;
+  public myForm: FormGroup = new FormGroup({
+    state: this.stateCtrl
+  });
+  public selected: string = '';
+  public dataSource: Observable<any>;
+  public asyncSelected: string = '';
+  public typeaheadLoading: boolean = false;
+  public typeaheadNoResults: boolean = false;
 
   constructor(
     private _apiSearchService: ApiSearchService,
   ) {
+    this.dataSource = Observable.create((observer: any) => {
+      // Runs on every search
+      if (this.asyncSelected.length > 0) {
+        this._apiSearchService.find({
+          query: {
+            title: {
+              $search: this.asyncSelected
+            }
+          }
+        }).then(res => {
+          observer.next(res.data);
+        });
+      }
+      //observer.next(this.asyncSelected);
+    }).mergeMap((res: any) => Observable.of(res));
+  }
+
+
+  public changeTypeaheadLoading(e: boolean): void {
+    this.typeaheadLoading = e;
+  }
+
+  public changeTypeaheadNoResults(e: boolean): void {
+    this.typeaheadNoResults = e;
+  }
+
+  public typeaheadOnSelect(e: TypeaheadMatch): void {
+    console.log('Selected value: ', e.value);
   }
 
   ngOnInit() {
-    this.term$.subscribe(term => this.search(term));
+
   }
 
-  search(term: string) {
-    this._apiSearchService.find(term).then(result => {
-      if (result.data) {
-        this.tracks = result.data;
-      }
-    });
+  onSubmit(event) {
+
   }
 }
--- src/app/shared/services/api/api-search.service.ts
+++ src/app/shared/services/api/api-search.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/Observable';
 import {Injectable} from '@angular/core';
 import { RestService } from './../rest.service';
 
