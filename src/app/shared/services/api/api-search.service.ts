6ecf6f29fa7ae197cd193064586253a965a49776
Berich <sarra.berrich@gmail.com>
2016-12-04 21:21:18 +0100

    fix bug: distinctUntilChange does not exist

--- src/app/shared/services/api/api-search.service.ts
+++ src/app/shared/services/api/api-search.service.ts
@@ -1,5 +1,5 @@
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 import { Track } from './../../models/track';
 
 import { Injectable } from '@angular/core';
