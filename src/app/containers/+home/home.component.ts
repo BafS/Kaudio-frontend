0a5b1dd5bd487ecd561fc40c5ab0731f0a3308c6
Fred <frederic.fyfer@gmail.com>
2017-01-06 15:00:33 +0100

    Clean HomeComponent

--- src/app/containers/+home/home.component.ts
+++ src/app/containers/+home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from './../../services/api/authentication.service';
 // import { PlaylistComponent } from './../shared/components/playlist/playlist';
 
 @Component({
@@ -7,14 +6,10 @@ import { AuthenticationService } from './../../services/api/authentication.servi
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   // directives: [ PlaylistComponent ],
-  providers: [ AuthenticationService ]
 })
 export class HomeComponent implements OnInit {
 
-  token = this._authService.getToken()
-
   constructor(
-    private _authService: AuthenticationService
     // private router: Router
   ) {
   }
