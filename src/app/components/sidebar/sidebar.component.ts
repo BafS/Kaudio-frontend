5fb1fc804584902957bb1e72a5233e1d5d6ee83e
Fred <frederic.fyfer@gmail.com>
2017-01-10 18:48:10 +0100

    Correct sidebar logout bug

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { ActionReducer, Action, Store } from '@ngrx/store';
 
 import { Playlist } from '../../models/playlist';
 
@@ -11,6 +12,14 @@ import { Playlist } from '../../models/playlist';
 
 // TODO Verifier si après 5 ou plus playlists elles s'affichent dans le sidebar...
 export class SidebarComponent {
+  public login$: Observable<boolean>;
+
+  constructor(
+    private _store: Store<any>,
+  ) {
+    this.login$ = _store.select(s => s.login);
+  }
+
   @Input('playlists') playlistsList: Observable<Playlist[]>;
   @Output('selectPlaylist') selectedPlaylist = new EventEmitter<string>();
   currentPlaylistId: string = '';
