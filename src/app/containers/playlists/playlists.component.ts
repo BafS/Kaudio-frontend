4a34e3cd25bd8f95005333df4c88d00a003b1600
BafS <fabacrans@gmail.com>
2017-01-11 10:04:31 +0100

    Redirect to login of not connected

--- src/app/containers/playlists/playlists.component.ts
+++ src/app/containers/playlists/playlists.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MdDialogRef, MdDialogConfig, MdDialog } from '@angular/material';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { Store } from '@ngrx/store';
@@ -25,6 +26,7 @@ export class PlaylistsComponent implements OnInit, OnDestroy {
   playlist$: Observable<Playlist>;
 
   public playlistsStore: Observable<fromPlaylists.State>;
+  public loginStore: Observable<any>;
   public playlistsEntitiesStore: Observable<Playlist[]>;
 
   private _playlistSelectSubscription: Subscription;
@@ -33,10 +35,12 @@ export class PlaylistsComponent implements OnInit, OnDestroy {
 
   constructor(
     private _playlistService: PlaylistService,
+    private _router: Router,
     private _store: Store<any>,
     public dialog: MdDialog
   ) {
     this.playlistsStore = _store.select(s => s.playlists);
+    this.loginStore = _store.select(s => s.login);
     this.playlistsEntitiesStore = _store.select(s => s.playlists.entities);
 
     const getSelectedPlaylist = createSelector(getPlaylistState, fromPlaylists.getSelected);
@@ -45,37 +49,44 @@ export class PlaylistsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this._playlistSelectSubscription = this._store.select(s => s.playlists.selectedPlaylistId).subscribe((id: string) => {
-        if (id) {
-          this._playlistService.get(id).then(playlist => {
-            console.log(playlist);
+    this.loginStore.map(p => p.connected).subscribe((connected: boolean) => {
+      if (connected) {
+        this._playlistService.find({query: {
+            $limit: 100
+          }
+        }).then(playlists => {
+          console.info(playlists);
 
+          // If a playslist exists
+          if (playlists.data) {
             this._store.dispatch({
-              type: fromPlaylists.ActionTypes.LOAD_PLAYLIST,
-              payload: playlist
+              type: fromPlaylists.ActionTypes.INDEX_PLAYLISTS,
+              payload: playlists.data
             });
-          });
-        }
+
+            // Reload previous playlist if possible
+            let id = window.localStorage.getItem('playlist-key');
+            if (id) {
+              this.selectPlaylist(id);
+            }
+          }
+        });
+      } else {
+        console.log('Not connected, redirect to login');
+        this._router.navigate(['login']);
+      }
     });
 
-    this._playlistService.find({query: {
-        $limit: 100
-      }
-    }).then(playlists => {
-      console.info(playlists);
+    this._playlistSelectSubscription = this._store.select(s => s.playlists.selectedPlaylistId).subscribe((id: string) => {
+      if (id) {
+        this._playlistService.get(id).then(playlist => {
+          console.log(playlist);
 
-      // If a playslist exists
-      if (playlists.data) {
-        this._store.dispatch({
-          type: fromPlaylists.ActionTypes.INDEX_PLAYLISTS,
-          payload: playlists.data
+          this._store.dispatch({
+            type: fromPlaylists.ActionTypes.LOAD_PLAYLIST,
+            payload: playlist
+          });
         });
-
-        // Reload previous playlist if possible
-        let id = window.localStorage.getItem('playlist-key');
-        if (id) {
-          this.selectPlaylist(id);
-        }
       }
     });
   }
